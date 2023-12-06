a35edc45b6fb27e1bc84eabf28621902b256dda1
Viper Muffin <vipermuffin@gmail.com>
2023-12-06 00:03:52 -0600

    Initial Day 06 Solution

---
+++ Day06/Day06.h
@@ -0,0 +1,16 @@
+//
+//  Advent of Code 2023 Solutions: Day 6
+//
+//  https://adventofcode.com/2023
+//
+//  Created by vipermuffin on 12/05/2023.
+//  Copyright © 2023 vipermuffin. All rights reserved.
+//
+#pragma once
+#include <string>
+#include <vector>
+
+namespace AocDay06 {
+//Function Definitions
+int64_t calcNumWins(int64_t time, int64_t dist, int32_t accel = 1);
+}
