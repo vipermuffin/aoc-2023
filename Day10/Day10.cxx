7bf3fdb6eda5d2e8483e378e53f75dcdf5d23f7a
vipermuffin <vipermuffin@gmail.com>
2023-12-12 21:04:59 -0600

    Update Day10.cxx

--- Day10/Day10.cxx
+++ Day10/Day10.cxx
@@ -11,25 +11,8 @@
 #include "AoCUtils.h"
 //Common Libraries
 #include <algorithm> //std::sort, find, for_each, max_element, etc
-//#include <array>
 #include <climits>   //INT_MIN, INT_MAX, etc.
-//#include <chrono>
-//#include <iostream>
-//#include <fstream> //ifstream
-//#include <functional> //std::function
-//#include <iomanip> //setfill setw hex
-//#include <map>
-//#include <math.h> //sqrt
-//#include <numeric> //std::accumulate
-//#include <queue>
-//#include <regex>
-//#include <set>
-//#include <sstream>
-//#include <thread>
-//#include <tuple>
 #include <unordered_map>
-//#include <unordered_set>
-
 
 using namespace std;
 namespace AocDay10 {
